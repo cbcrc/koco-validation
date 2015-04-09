7eb276907146bf0c96047e749752a5c307fd6618
Allov <frederik.charest@gmail.com>
2015-04-09 14:52:17 -0400

    licensing

--- src/binding-handlers/form-group-validation-css-class.js
+++ src/binding-handlers/form-group-validation-css-class.js
@@ -1,4 +1,7 @@
-﻿define(['knockout', 'jquery'], function (ko, $) {
+﻿// Copyright (c) CBC/Radio-Canada. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+define(['knockout', 'jquery'], function (ko, $) {
     "use strict";
 
     ko.bindingHandlers['formGroupValidationCssClass'] = {
