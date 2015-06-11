52fa96a08bae5362c4f061b66e9c5d940894e011
Mark Moyes <mark.moyes@cbc.ca>
2015-06-11 07:27:47 -0700

    Added extender to trigger validation on another observable.

--- src/general-validation-rules.js
+++ src/general-validation-rules.js
@@ -14,4 +14,17 @@ define(['knockout'], function(ko) {
         },
         message: 'The field must not equal {0}'
     };
+
+    // technically not a validation rule, this extender simply triggers validation
+    // on another observable. Useful for validating a range of dates, for example,
+    // where a change to startDate could cause endDate's dateIsAfter rule to be validated.
+    ko.extenders.triggerValidationOn = function(target, dependency) {
+        target.subscribe(function() {
+            // force knockout validation to rerun validation on dependency
+            ko.validation.validateObservable(dependency);
+            // update UI (validate() is defined in validation-utilities.js)
+            dependency.validate();
+        });
+        return target;
+    };
 });
